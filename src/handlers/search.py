193c7fff3aef6fee83504c33029c3bd7b160f103
payorayo <payorayo@gmail.com>
2022-05-17 15:23:04 -0600

    Feat: get ingredients

--- src/handlers/search.py
+++ src/handlers/search.py
@@ -6,11 +6,12 @@ from aws_lambda_powertools import Logger
 from src.data import enums
 from src.services import validation
 from src.services.config import ConfigService
+from src.services.db import DB
 from src.services.util import describe_db_instances
 from src.services.response import ResponseService
 
-conf_service = ConfigService()
-logger = Logger(service=conf_service.LOGGER_SERVICE_NAME)
+conf = ConfigService()
+logger = Logger(service=conf.LOGGER_SERVICE_NAME)
 
 
 @ResponseService.pretty_response
@@ -29,11 +30,19 @@ def handler(event, _):
     logger.info({"message": "RDS", "info": response})
     endpoint = response["DBInstances"][0].get("Endpoint", {})
 
-    body = {
-        "owner_id": owner_id,
-        "name": name,
-        "host": endpoint.get("Address"),
-        "port": endpoint.get("Port")
-    }
+    db = DB(db_name=conf.DB_NAME, host=endpoint.get("Address"), port=endpoint.get("Port"),
+            username=conf.DB_USERNAME, password=conf.DB_USER_PASSWORD)
 
-    return HTTPStatus.OK, body
+    items = db.search_ingredients_by_name(name=name, owner_id=owner_id)
+    logger.info({"message": "ITEMS", "info": items})
+    response = []
+    for item in items:
+        response.append({
+            "id": item["id"],
+            "name": item["name"],
+            "owner_id": item["owner_id"]
+        })
+
+    return HTTPStatus.OK, {
+            "ingredients": response
+        }
